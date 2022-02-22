2849d871921ce71a85a7080abdb9cacab1b9537c
Mitsufumi Kikyotani <mikikyotani@deloitte.com.au>
2022-02-23 10:06:53 +1100

    fix: use terraform backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = "= 1.1.6"
+  backend "s3" {
+    bucket = "kikyomits-terraform"
+    key = "tfstate"
+    region = "ap-southeast-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
